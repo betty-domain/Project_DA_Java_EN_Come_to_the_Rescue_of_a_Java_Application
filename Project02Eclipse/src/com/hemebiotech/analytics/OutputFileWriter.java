3aee829f0a57a7cc730df9e5d269e3070ef6df83
betty Domain <betty.domain@free.fr>
2020-11-23 17:15:03 +0100

    Implémentation de la classe OutputFileWriter pour lécriture d'une map dans un fichier de sortie

--- Project02Eclipse/src/com/hemebiotech/analytics/OutputFileWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/OutputFileWriter.java
@@ -1,7 +1,9 @@
 package com.hemebiotech.analytics;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class OutputFileWriter {
 
@@ -9,9 +11,26 @@ public class OutputFileWriter {
      * Write a Map into an output File with sorted elements by natural order (alphabetical)
      * @param mapToWrite map to Write, can have unsorted elements
      */
-    public static void WriteInFile(Map<String,Long> mapToWrite)
-    {
-        //TODO implement FileWriter method
+    public static void WriteInFile(Map<String,Long> mapToWrite) {
 
+        try {
+            FileWriter writer = new FileWriter("result.out");
+
+            // TreeMap is used to order elements by natural order (alphabetical)
+            TreeMap<String, Long> sorted = new TreeMap<>(mapToWrite);
+
+            // Loop on each element in the TreeMap and write the key value elements in file
+            sorted.forEach((key,value) -> {
+                        try {
+                            writer.write(key + ": " + value.toString() + "\n");
+                        } catch (IOException ioException) {
+                            System.out.println("An error occurs when writing in output file. Error Message : " + ioException.getMessage());
+                        }
+                    });
+
+            writer.close();
+        } catch (IOException ioWriteException) {
+            System.out.println("An error occurs during output file creation. Error Message : " + ioWriteException.getMessage());
+        }
     }
 }
