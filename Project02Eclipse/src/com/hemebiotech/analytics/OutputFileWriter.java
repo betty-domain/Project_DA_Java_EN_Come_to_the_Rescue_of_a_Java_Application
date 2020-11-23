a4dddd008ed5f247b04ced2689c685d2d1e5aa69
betty Domain <betty.domain@free.fr>
2020-11-23 12:09:34 +0100

    Création de la structure du programme avec découpage des classes et méthodes. Création d'une méthode main dans la classe LaunchProgram depuis laquelle on appelle uniquement les méthodes souhaitées. Création d'une classe FileWriter pour l'écriture des résultats dans un fichier. Modification de la classe Analytics Counter pour la dédiée au décompte des symptômes uniquement.

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/OutputFileWriter.java
@@ -0,0 +1,17 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.util.Map;
+
+public class OutputFileWriter {
+
+    /**
+     * Write a Map into an output File with sorted elements by natural order (alphabetical)
+     * @param mapToWrite map to Write, can have unsorted elements
+     */
+    public static void WriteInFile(Map<String,Integer> mapToWrite)
+    {
+        //TODO implement FileWriter method
+
+    }
+}
