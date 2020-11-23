6e2adf3c1eed0cfac9e9c05318a859f6881a2990
betty Domain <betty.domain@free.fr>
2020-11-23 14:26:26 +0100

    Implémentation du décompte des symptômes et modification de la signature de la méthode dans la classe Launcher pour passer d'un objet Integer à un objet Long dans la Map retournée

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,50 +3,11 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-
-		//lecture du fichier des symptômes
-		BufferedReader reader = new BufferedReader (new FileReader("../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
 
 	/**
 	 * If no data is available, return an empty Map
@@ -56,9 +17,15 @@ public class AnalyticsCounter {
 	 *  as value : number of occurs in the List of the string key
 	 */
 
-	public static Map<String,Integer> CountStringOccurences(List<String> listStrings)
+	public static Map<String,Long> CountStringOccurences(List<String> listStrings)
 	{
-		//TODO : IMPLEMENT counter of List of strings into a Map
-		return 	null;
+		Map<String,Long> mapResult = new HashMap<>();
+
+		if (listStrings!=null)
+		{
+			//browse the list of strings, grouping by string and counting the number of occurrences in the list
+			mapResult = listStrings.stream().collect(Collectors.groupingBy(str -> str, Collectors.counting()));
+		}
+		 return  mapResult;
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/OutputFileWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/OutputFileWriter.java
@@ -9,7 +9,7 @@ public class OutputFileWriter {
      * Write a Map into an output File with sorted elements by natural order (alphabetical)
      * @param mapToWrite map to Write, can have unsorted elements
      */
-    public static void WriteInFile(Map<String,Integer> mapToWrite)
+    public static void WriteInFile(Map<String,Long> mapToWrite)
     {
         //TODO implement FileWriter method
 
