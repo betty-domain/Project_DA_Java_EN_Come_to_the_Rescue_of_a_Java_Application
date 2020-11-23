f1bea949c240b3627450c7e0998d48d80602766d
betty Domain <betty.domain@free.fr>
2020-11-23 12:09:34 +0100

    Création de la structure du programme avec découpage des classes et méthodes. Création d'une méthode main dans la classe LaunchProgram depuis laquelle on appelle uniquement les méthodes souhaitées. Création d'une classe FileWriter pour l'écriture des résultats dans un fichier. Modification de la classe Analytics Counter pour la dédiée au décompte des symptômes uniquement.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,6 +3,12 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
@@ -40,5 +46,19 @@ public class AnalyticsCounter {
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+	}
+
+	/**
+	 * If no data is available, return an empty Map
+	 *
+	 * @return a map containing
+	 * 	as key : each different string in List used in parameter
+	 *  as value : number of occurs in the List of the string key
+	 */
+
+	public static Map<String,Integer> CountStringOccurences(List<String> listStrings)
+	{
+		//TODO : IMPLEMENT counter of List of strings into a Map
+		return 	null;
 	}
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/LaunchProgram.java
@@ -0,0 +1,24 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public class LaunchProgram {
+
+    public static void main(String args[]) throws Exception {
+
+        try {
+            ReadSymptomDataFromFile symptomReader = new ReadSymptomDataFromFile("../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt");
+
+            List<String> symptomsList = symptomReader.GetSymptoms();
+
+            Map<String, Integer> symptomsMap = AnalyticsCounter.CountStringOccurences(symptomsList);
+
+            OutputFileWriter.WriteInFile(symptomsMap);
+        }
+        catch (Exception exception)
+        {
+            System.out.println("An error occurs, see details below : " + exception.getMessage());
+        }
+    }
+}
